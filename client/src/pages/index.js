de43413313d71475151755cb466a77710ac23bcc
Chris Mullendore <chris@vindage.com>
2022-07-31 23:07:03 -0700

    Remove unneeded reference

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,6 +4,5 @@ import Profile from './Profile';
 import ReadBooks from './ReadBooks';
 import SearchBooks from './SearchBooks';
 import SavedBooks from './SavedBooks';
-import EmailConfirmed from './EmailConfirmed';
 
-export { Home, Profile, ReadBooks, SearchBooks, SavedBooks, EmailConfirmed }
+export { Home, Profile, ReadBooks, SearchBooks, SavedBooks }
